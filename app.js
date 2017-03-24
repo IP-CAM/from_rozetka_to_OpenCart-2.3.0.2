d17cee732e601a771833942960d7bcfef65382dd
tolik555 <tolik555.83@gmail.com>
2017-03-24 22:05:16 +0200

    Первые шаги сделаны))
    Установлен express body-parser tress needle cherio nodemon mysql

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express'); // подключение фреймворка express
+var bodyParser = require('body-parser');//подключение библиотеки получения пост
+var tress = require('tress');// Простая в использовании асинхронная очередь заданий
+var needle = require('needle');// Самый маленький и самый красивый клиент HTTP
+var cheerio = require('cheerio');//jQuery для node.js
+                                //nodemon библиотека для автоматического перезапуска сервера при изменении файлов
+                                //mysql Библиотека для работы с бд
+
+
