c23cde1876052e1fa2db23df1d9afb2d01190773
tolik555 <tolik555.83@gmail.com>
2017-03-28 15:44:34 +0300

    Описание таблиц базы данных

---
+++ dbDesc.js
@@ -0,0 +1,83 @@
+
+var result_oc_product_description = {
+    "product_id": '',
+    "language_id": 1,
+    "name": '',
+    "description": '',
+    "tag": '',
+    "meta_title": '',
+    "meta_description": '',
+    "meta_keyword": ''
+};
+
+var result_oc_product = {
+    "product_id": '', //autoincrement
+    "model": '',
+    "sku": '',
+    "upc": '',
+    "ean": '',
+    "jan": '',
+    "isbn": '',
+    "mpn": '',
+    "location": '',
+    "quantity": '990',
+    "stock_status_id": 5,
+    "image": '123',
+    "manufacturer_id": '',
+    "shipping": 1,
+    "price": 0,
+    "points": 0,
+    "tax_class_id": 9,
+    "date_available": '2017-01-01',
+    "weight": 0,
+    "weight_class_id": 1,
+    "length": 0,
+    "width": 0,
+    "height": 0,
+    "length_class_id": 1,
+    "subtract": 1,
+    "minimum": 0,
+    "sort_order": 0,
+    "status": 1,
+    "viewed": 0,
+    "date_added": '',
+    "date_modified": ''
+};
+
+var result_oc_product_attribute = {
+    "product_id": '',
+    "attribute_id": '',
+    "language_id": '',
+    "text": ''
+};
+
+var result_oc_product_image = {
+    "product_image_id": '', //autoincrement
+    "product_id": '',
+    "image": '',
+    "sort_order": 0
+};
+
+var result_oc_product_option = {
+    "product_option_id": '', //autoincrement
+    "product_id": '',
+    "option_id": '',
+    "value": '',
+    "required": ''
+};
+
+var result_oc_product_to_category = {
+    "product_id": '',
+    "category_id": 18
+};
+
+var result_oc_product_to_layout = {
+    "product_id": '',
+    "store_id": 0,
+    "layout_id": 0
+};
+
+var result_oc_product_to_store = {
+    "product_id": '',
+    "store_id": 0
+};
